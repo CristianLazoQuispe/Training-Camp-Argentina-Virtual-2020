50441ad1fb003221ef4fc9f8e4436c62f4451ae4
Cristian Lazo Quispe <mecatronico.lazo@gmail.com>
2020-07-09 12:05:37 -0500

    watermelon solution

---
+++ Contest/Warm_up/A-watermelon.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int w;
+int main() {
+	// your code goes here
+	cin>>w;
+	if (w%2==0 && w > 2){
+	    cout<<"YES";
+	}
+	else{
+	    cout<<"NO";
+	}
+	return 0;
+}
